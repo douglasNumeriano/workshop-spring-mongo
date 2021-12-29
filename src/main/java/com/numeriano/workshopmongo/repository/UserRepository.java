c2d09327d3d33f42293d269418c07a649c3ec1a9
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-29 19:22:08 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/numeriano/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.numeriano.workshopmongo.repository;
+
+import com.numeriano.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
--- src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
@@ -1,11 +1,25 @@
 package com.numeriano.workshopmongo.resources;
 
+import com.numeriano.workshopmongo.domain.User;
+import com.numeriano.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value= "/users")
 public class UserResource {
 
-    
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
 }
