c2b5e85b1515d2855bb6d797297ec813acb6f3fd
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-30 10:58:42 -0300

    Obtendo um usuário por id

--- src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.numeriano.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> usersDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(usersDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
---
+++ src/main/java/com/numeriano/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.numeriano.workshopmongo.resources.exception;
+
+import com.numeriano.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+//Esta anotação indica que essa classe vai tratar possíveis erros nas requisições
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
