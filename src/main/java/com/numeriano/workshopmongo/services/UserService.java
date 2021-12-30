c2f71b6f5198e662eedb3783f73af307f2433f14
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-30 13:50:50 -0300

    Inserção de usuário com POST

--- src/main/java/com/numeriano/workshopmongo/services/UserService.java
+++ src/main/java/com/numeriano/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.numeriano.workshopmongo.services;
 
 import com.numeriano.workshopmongo.domain.User;
+import com.numeriano.workshopmongo.dto.UserDTO;
 import com.numeriano.workshopmongo.repository.UserRepository;
 import com.numeriano.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
        Optional<User> user = repository.findById(id);
        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
