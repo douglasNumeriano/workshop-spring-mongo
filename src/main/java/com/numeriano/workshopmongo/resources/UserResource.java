a1be92a4f5a880d984df88ba641887f7833d07d3
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-30 14:10:11 -0300

    Atualização de usuário com PUT

--- src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/numeriano/workshopmongo/resources/UserResource.java
@@ -40,9 +40,17 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
-    @DeleteMapping
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
